d4a75921ab98cc94fbfaf9388e927c5c02915d33
shubham2419 <f20170363@goa.bits-pilani.ac.in>
2018-09-18 20:41:01 +0530

    3st commit

--- Basic.js
+++ Basic.js
@@ -1,8 +1,38 @@
 
 	document.writeln("<h1> HI WELCOME</h1>");
-	console.log("Hello console")
+	console.log("Hello console");
 
 	var i =9;
 	var name="Shubham"
 	var array1 = ["hello","world","hello"];
+	console.log(i);
+	var b=i+5;
+	console.log(b);
+	console.log(typeof(i));
+	//coment
+	/*multi
+	line comment*/
+
+	function myfunction1(){
+		console.log("Hello World");
 	
+	}
+	var a,b;
+	function myfunction2(a,b){
+		return a+b;
+	}
+	function myfunction3(a,b){
+		return a-b;
+	}
+	function myfunction4(a,b){
+		return a/b;
+	}
+	function myfunction5(a,b){
+		return a*b;
+	}
+	myfunction1();
+	
+	console.log(myfunction2(1,2));
+	console.log(myfunction3(1,2));
+	console.log(myfunction4(1,2));
+	console.log(myfunction5(1,2));	
